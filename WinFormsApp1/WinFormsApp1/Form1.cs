8d366d4e64a4111f12eeae4d3b1b70f78a1dc184
Afjal13 <afjal.hossain12496@gmail.com>
2024-01-17 22:47:53 +0600

    Insert data

--- WinFormsApp1/WinFormsApp1/Form1.cs
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -1,7 +1,11 @@
+using System.Data.SqlClient;
+
 namespace WinFormsApp1
 {
     public partial class EmployeeForm : Form
     {
+        string connectionString = "Data Source=MSI\\SQLEXPRESS;Initial Catalog=EmployeeManagement;Integrated Security=True;";
+
         public EmployeeForm()
         {
             InitializeComponent();
@@ -9,7 +13,41 @@ namespace WinFormsApp1
 
         private void btnSave_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Wellcome!");
+            string empId = txtId.Text;
+            string empName = txtName.Text;
+            int empSalary = Convert.ToInt32(txtSalary.Text);
+            string empCity = txtCity.Text;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    string insertQuery = "INSERT INTO EmployeeDetails (empId, empName, empSalary,empCity) VALUES (@empId, @empName, @empSalary,@empCity)";
+
+                    using (SqlCommand command = new SqlCommand(insertQuery, connection))
+                    {
+                        // Add parameters
+                        command.Parameters.AddWithValue("@empId", empId);
+                        command.Parameters.AddWithValue("@empName", empName);
+                        command.Parameters.AddWithValue("@empSalary", empSalary);
+                        command.Parameters.AddWithValue("@empCity", empCity);
+
+                        // Execute the query
+                        command.ExecuteNonQuery();
+                        Application.Exit();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
         }
     }
 }
